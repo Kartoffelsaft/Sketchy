20ca516a9c333680a209a5a8966d4712d7c6af7e
Ben Findley <ben.g.findley@gmail.com>
2020-10-29 17:19:11 -0700

    list available languages

--- sketchy.sh
+++ sketchy.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# Note: Sketchy is designed with the assumtion that it is sourced; i.e.
+# ". ./sketchy.sh" instead of "./sketchy.sh"
 
 ###
 # User Config
@@ -13,8 +16,23 @@ SKETCHY_DEFAULT_EDITOR=nvim
 
 # Aliases
 
+declare -A SKETCHY_VALID_LANGS=(
+    ["cpp"]='c++ cpp'
+    ["rust"]='rust rs'
+    ["haskell"]='haskell hs'
+)
+
 SKETCHY_LANG=$1
 SKETCHY_PROJECT_NAME=$2
 
 SKETCHY_PROJECT_DIR="$SKETCHY_ROOT_DIR$SKETCHY_PROJECT_NAME"
+
+# Project Generation
+
+for langName in ${SKETCHY_VALID_LANGS[*]}
+do
+    echo $langName
+done
+
 mkdir $SKETCHY_PROJECT_DIR
+cd $SKETCHY_PROJECT_DIR
